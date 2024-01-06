771474107b8b32a9c7bc784c624f4c51147b97bd
Lanan-s <145327806+Lanan-s@users.noreply.github.com>
2024-01-06 18:24:11 +0800

    Add files via upload

---
+++ vue.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+var app = new Vue({
+    el: "#sec4-app",
+    data: {
+        city: "",
+        weatherList: [],
+    },
+    methods: {
+        searchEnter: function () {
+            var that = this;
+            axios
+                .get("http://wthrcdn.etouch.cn/weather_mini?city=" + this.city)
+                .then(function (response) {
+                    that.weatherList = response.data.data.forecast;
+                })
+                .catch(function (err) {});
+        },
+        changeCity: function (city) {
+            this.city = city;
+            this.searchEnter();
+        },
+        clickButton: function () {
+            this.searchEnter();
+        },
+    },
+});
